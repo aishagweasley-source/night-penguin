9614880ffa719a05a4bc4ad8b6d2433261a51ddd
noonafter <aisha.gweasley@gmail.com>
2026-04-05 23:31:39 +0700

    Add files via upload

---
+++ night-penguin.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo myservo1;
+Servo myservo2;
+
+#define servoRight 3
+#define servoLeft 5
+#define button1 6
+#define button2 9
+
+void setup() {
+  myservo1.attach(servoRight);
+  myservo2.attach(servoLeft);
+
+  pinMode(button1, )
+}
+
+
+
+
